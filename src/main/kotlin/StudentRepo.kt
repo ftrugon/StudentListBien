34bb30812c9e32c4c28619951124dc92ae9dceb9
ftrugon <fjtrujillo@iesrafaelalberti.es>
2024-05-07 19:48:46 +0200

    un par de cositas mas

---
+++ src/main/kotlin/StudentRepo.kt
@@ -0,0 +1,3 @@
+class StudentRepo:IStudentRepo   {
+
+}
---
+++ src/main/kotlin/ViewModelDb.kt
@@ -0,0 +1,34 @@
+class ViewModelDb:IViewModel {
+    override fun anadirAlumno() {
+        TODO("Not yet implemented")
+    }
+
+    override fun guardarCambios() {
+        TODO("Not yet implemented")
+    }
+
+    override fun eliminarAlumno(index: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun eliminarTodos() {
+        TODO("Not yet implemented")
+    }
+
+    override fun cambiarNombreAlumno(index: Int, nuevoNombre: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun obtenerAlumnos(): MutableList<String> {
+        TODO("Not yet implemented")
+    }
+
+    override fun obtenerNuevoAlumno(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun cambiarNuevoAlumno(nuevoNombre: String) {
+        TODO("Not yet implemented")
+    }
+
+}
